709380adc66fccc0a1fb450b114446e4e261bed9
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-07-17 15:45:38 -0300

    Create CSV Parse Function, apply pipe Stream concepts on UseCase, consuming the data for request

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,15 @@
+import fs from 'fs'
+import { parse as csvParse } from 'csv-parse'
+
+export class ImportCategoryUseCase {
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path)
+
+    const parseFile = csvParse()
+    stream.pipe(parseFile)
+
+    parseFile.on("data", async (line) => {
+      console.log(line)
+    })
+  }
+}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express'
 import { createCategoryController } from '../modules/cars/useCases/CreateCategory'
 import { listCategoryController } from '../modules/cars/useCases/listCategories'
 import multer from 'multer'
+import { importCategoryController } from '../modules/cars/useCases/importCategory'
 
 const categoriesRoutes = Router()
 const upload = multer({
@@ -17,10 +18,8 @@ categoriesRoutes.get('/', (req: Request, res: Response) => {
 })
 
 categoriesRoutes.post('/import', upload.single("file"), (req: Request, res: Response) => {
+  return importCategoryController.handle(req, res)
 
-  const { file } = req
-  console.log(file)
-  return res.status(201).send()
 })
 
 export { categoriesRoutes }
