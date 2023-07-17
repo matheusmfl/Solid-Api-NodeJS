9a440fd4381588e5dfd19cb4c9b1c9b1d6e33b24
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-07-16 23:01:16 -0300

    Create CSV UPLOAD Archies, install Multer Dependencie

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,12 @@
 import { Request, Response, Router } from 'express'
 import { createCategoryController } from '../modules/cars/useCases/CreateCategory'
 import { listCategoryController } from '../modules/cars/useCases/listCategories'
+import multer from 'multer'
 
 const categoriesRoutes = Router()
+const upload = multer({
+  dest: './tmp'
+})
 
 categoriesRoutes.post('/', (req: Request, res: Response) => {
   return createCategoryController.handle(req, res)
@@ -10,6 +14,13 @@ categoriesRoutes.post('/', (req: Request, res: Response) => {
 
 categoriesRoutes.get('/', (req: Request, res: Response) => {
   return listCategoryController.handle(req, res)
+})
+
+categoriesRoutes.post('/import', upload.single("file"), (req: Request, res: Response) => {
+
+  const { file } = req
+  console.log(file)
+  return res.status(201).send()
 })
 
 export { categoriesRoutes }
