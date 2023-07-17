1dbf3a9310f9596908c79debb7537b470715dc63
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-07-17 16:19:43 -0300

    Create Async Promises for tratative Archies, using new Method LoadCategories and execute asyncrhone

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,15 +1,42 @@
 import fs from 'fs'
 import { parse as csvParse } from 'csv-parse'
+import { ICategoryRepository } from '../../repositories/implementations/ICategoriesRepository'
 
+
+interface IImportCategory {
+  name: string
+  description: string
+}
 export class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path)
 
-    const parseFile = csvParse()
-    stream.pipe(parseFile)
+  constructor(private categoriesRepository: ICategoryRepository) { }
 
-    parseFile.on("data", async (line) => {
-      console.log(line)
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path)
+      const categories: IImportCategory[] = []
+      const parseFile = csvParse()
+      stream.pipe(parseFile)
+
+      parseFile.on("data", async (line) => {
+        const [name, description] = line;
+        categories.push({
+          name,
+          description
+        })
+      })
+        .on("end", () => {
+          resolve(categories)
+        })
+        .on("error", (err) => {
+          reject(err)
+        })
     })
   }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+    console.log(categories)
+  }
+
 }
